9ffdd0d7075fea9d32853cfc28e33cbac3f82354
mderosa777 <meggsnbacon777@gmail.com>
2020-11-09 20:52:29 -0600

    hi

--- datingapp/app/controllers/api/v1/like_controller.rb
+++ datingapp/app/controllers/api/v1/like_controller.rb
@@ -1,16 +1,11 @@
-class LikeController < ApplicationController
+class Api::V1::LikeController < ApplicationController
     def index
         @likes = Like.all 
     end
-
     def show
         @like = Like.find(params[:id])
     end
-
     def new 
         @like = Like.new
     end
-
-    
 end
-
